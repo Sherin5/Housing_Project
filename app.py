990c526c042a2dafa8e6a11405a289490e3d9a92
Sherin5 <sherintrades@gmail.com>
2022-09-30 12:42:24 +0530

    CI CD pipeline completed

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/', methods= ['POST', 'GET'])
 def index():
-    return "Starting Housing Prediction Project"
+    return "CI CD Pipeline completed"
 
 if __name__=="__main__":
     app.run(debug=True)
